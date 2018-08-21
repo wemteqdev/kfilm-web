ab7d373deb0889fb066bb54a9136dc44888d82a7
alex <alexander.vasiliev87@yandex.com>
2018-08-21 21:48:43 +1000

    removed unnessary files

---
+++ resources/assets/js/components/ImageListPopupComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
--- resources/views/admin/categories/fields.blade.php
+++ resources/views/admin/categories/fields.blade.php
@@ -1,4 +1,4 @@
-<div class="form-group col-sm-6">
+<div class="form-group col-sm-6" id="image_field_group">
     {!! Form::label('featured_image_id', 'Featured Image:') !!}
     {!! Form::text('featured_image_id', null, ['class' => 'form-control']) !!}
 
@@ -7,6 +7,8 @@
     <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#myModal">
       Select Image
     </button>
+
+    <example-component />
 
 </div>
 
@@ -40,4 +42,11 @@
     <a href="{!! route('admin.categories.index') !!}" class="btn btn-default">Cancel</a>
 </div>
 
-{!! view('admin.images._modal') !!}
+<script>
+    Vue.component('example-component', require('./components/ExampleComponent.vue'));
+
+    const image_field_group = new Vue({
+        el: '#image_field_group'
+    });
+
+</script>
