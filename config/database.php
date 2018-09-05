f146eaa706dd075c87bc19a0338d4fabf098e9e0
alex <alexander.vasiliev87@yandex.com>
2018-09-05 20:01:30 +1000

    queue jobs

--- config/database.php
+++ config/database.php
@@ -105,16 +105,13 @@ return [
     */
 
     'redis' => [
-
-        'client' => 'predis',
-
+        'cluster' => false,
         'default' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
             'database' => 0,
         ],
-
     ],
 
 ];
