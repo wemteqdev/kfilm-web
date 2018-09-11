6846429ca61bd4ea4033385efd13b1c08d4afdb2
alex <alexander.vasiliev87@yandex.com>
2018-09-12 06:14:04 +1000

    admin cache bursting

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -10,10 +10,10 @@ Vue.use(VueMaterial);
 Vue.component('image-list-popup', require('./components/ImageListComponent.vue'));
 Vue.component('video-list-popup', require('./components/VideoListComponent.vue'));
 Vue.component('series-list-popup', require('./components/SeriesListComponent.vue'));
-
 Vue.component('video-category-list', require('./components/video/CategoryListComponent.vue'));
 Vue.component('video-group-list', require('./components/video/GroupListComponent.vue'));
 Vue.component('video-tag-list', require('./components/video/TagListComponent.vue'));
+
 
 Vue.component('slide-tag-list', require('./components/slide/TagListComponent.vue'));
 
