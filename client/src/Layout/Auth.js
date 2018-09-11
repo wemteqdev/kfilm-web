3692f0695a1a5eb58f29882f56ef136e9d2430ff
acharushin81 <acharushin81@yandex.com>
2018-09-12 05:18:18 +1000

    video page

---
+++ client/src/Layout/Auth.js
@@ -0,0 +1,30 @@
+import { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+
+class Auth extends Component {
+    componentWillMount = () => {
+        setTimeout(()=>{
+            let isLoggedIn = this.props.login.user != null;
+            if (isLoggedIn === false) {
+                this.props.history.push('/')
+            }
+        }, 2000)
+    }
+
+    render() {
+        return (
+            this.props.login.user != null && this.props.children
+        );
+    }
+};
+
+const mapStateToProps = (state) => {
+    return {
+      login: state.login
+    }
+}
+
+  
+export default withRouter( connect(mapStateToProps, null)(Auth));
--- client/src/components/widgets/Video/index.js
+++ client/src/components/widgets/Video/index.js
@@ -6,23 +6,32 @@ class Video extends Component {
     render (){
         return (
             <section className="fullwidth-single-video">
-                <div className="flex-video widescreen">
-                    <div
-                        dangerouslySetInnerHTML={{
-                        __html: this.props.video.embed
-                        }}>
+                <div className="container">
+                    <div className="flex-video widescreen">
+                        <div
+                            dangerouslySetInnerHTML={{
+                            __html: this.props.video.embed
+                            }}>
+                        </div>
                     </div>
                 </div>
                 <div className='container'>
                     <div className="row">
                         <div className="col-12 text-center">
+                            <div className="mt-2 mb-5">
+                                <a className="btn button more-button">To see full video Click here</a>
+                            </div>
                             {
                                 this.props.type === "pro" && this.props.like && <button className="btn btn-primary like-button" onClick={this.toggleLike}><FontAwesomeIcon icon="heart" /> Like</button>
                             }
                             {
                                 this.props.type === "pro" && !this.props.like && <button className="btn btn-secondary like-button" onClick={this.toggleLike}><FontAwesomeIcon icon="heart" /> Like</button>
                             }
+                            
                             <div className="mt-5 mb-5">
+                                <div className="head-title">
+                                    <h4 className='borderBottom'>Recommended</h4>
+                                </div>
                                 <VideoList videos={this.props.video.related}/>
                             </div>
                         </div>
