0b0899930850bc4820f3ffee6b0f7332385b4b53
alex <alexander.vasiliev87@yandex.com>
2018-09-13 23:04:07 +1000

    missing dependencies for forgot password controller

--- app/Http/Controllers/API/Auth/ResetPasswordController.php
+++ app/Http/Controllers/API/Auth/ResetPasswordController.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers\API\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use App\Http\Resources\User as UserResource;
 
 class ResetPasswordController extends Controller
