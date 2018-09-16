5e4ff8b9eee2741c9db42a5557beaaf271671567
alex <alexander.vasiliev87@yandex.com>
2018-09-16 18:44:09 +1000

    user filtering in admin

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,13 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-    <section class="content-header">
-        <h1 class="pull-left">Users ({{ \App\User::count() }})</h1>
-        <h1 class="pull-right">
-           <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{!! route('admin.users.create') !!}">Add New</a>
-        </h1>
+    <section class="content-header clearfix mb-3">
+        <h2 class="pull-left">USERS ({{ \App\User::count() }})</h2>
     </section>
     <div class="content">
+        <div class="clearfix">
+            <form class="form-inline" action="{!! request()->fullUrlWithQuery([]) !!}">
+                <div class="form-group">
+                    <input type="text" name="search" class="form-control" value="{{ app('request')->input('search') }}"/>
+                    <input type="hidden" name="searchFields" value="name:like;email:like"/>
+                    <input type="submit" value="Submit" tabindex="-1" class="btn btn-primary"/>
+                </div>
+            </form>
+        </div>
+        <br/>
         <div class="clearfix"></div>
 
         @include('flash::message')
